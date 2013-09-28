f3dcd3792bc440b0bf70143e4b34e905f4cf4eed
Pascal Brokmeier <pascal.brokmeier@opitz-consulting.com>
2013-09-28 17:38:53 +0200

    if this doesnt work now i'll be done for today

--- RCSwitchJNIWrapper/src/NativeRCSwitchAdapter.cpp
+++ RCSwitchJNIWrapper/src/NativeRCSwitchAdapter.cpp
@@ -1,5 +1,5 @@
 #include "NativeRCSwitchAdapter.h"
-#include "RCSwitch-Pi/RCSwitch.h"
+#include "RCSwitch.h"
 #include <stdio.h>
 #include <iostream>
 
@@ -21,14 +21,31 @@ JNIEXPORT void JNICALL Java_NativeRCSwitchAdapter_sendBinary (JNIEnv *env, jobje
     env->ReleaseStringUTFChars(javaString, nativeString);
 }
 
-JNIEXPORT void JNICALL Java_NativeRCSwitchAdapter_switchOn(JNIEnv *, jobject, jstring jsGroup, jstring jsChannel )
+JNIEXPORT void JNICALL Java_NativeRCSwitchAdapter_switchOn(JNIEnv * env, jobject obj, jstring jsGroup, jstring jsChannel )
 {
-    const char *sGroup = env->GetStringUTFChars(jsGroup, 0);
-    const char *sChannel = env->GetStringUTFChars(jsChannel, 0);
+    cout<<"teststring output"<<endl;
+    const char *csGroup = env->GetStringUTFChars(jsGroup, 0);
+    const char *csChannel = env->GetStringUTFChars(jsChannel, 0);
+
+    char sGroup[6];
+    char sChannel[6];
+
+    for (int i = 0; i<5; i++) {
+        sGroup[i] = csGroup[i];
+        sChannel[i] = csChannel[i];
+    }
+    sGroup[5] = '\0';
+    sChannel[5] = '\0';
+
+    cout<<"ONON"<<endl;
+    cout<<sGroup<<endl;
+    cout<<sChannel<<endl;
+
+
     RCSwitch mySwitch = RCSwitch();
 
     //for testing purposes set to the ELRO Power Plugs
-    mySwitch.setPulseLength(300)
+    mySwitch.setPulseLength(300);
     mySwitch.enableTransmit(0);
     mySwitch.setRepeatTransmit(3);
 
@@ -37,14 +54,29 @@ JNIEXPORT void JNICALL Java_NativeRCSwitchAdapter_switchOn(JNIEnv *, jobject, js
 
 }
 
-JNIEXPORT void JNICALL Java_NativeRCSwitchAdapter_switchOff(JNIEnv *, jobject, jstring jsGroup, jstring jsChannel )
+JNIEXPORT void JNICALL Java_NativeRCSwitchAdapter_switchOff(JNIEnv * env, jobject obj, jstring jsGroup, jstring jsChannel )
 {
-    const char *sGroup = env->GetStringUTFChars(jsGroup, 0);
-    const char *sChannel = env->GetStringUTFChars(jsChannel, 0);
+    const char *csGroup = env->GetStringUTFChars(jsGroup, 0);
+    const char *csChannel = env->GetStringUTFChars(jsChannel, 0);
+
+    char sGroup[6];
+    char sChannel[6];
+
+    for (int i = 0; i<5; i++) {
+        sGroup[i] = csGroup[i];
+        sChannel[i] = csChannel[i];
+    }
+    sGroup[5] = '\0';
+    sChannel[5] = '\0';
+
+    cout<<"OFFOFF"<<endl;
+    cout<<sGroup;
+    cout<<sChannel;
+
     RCSwitch mySwitch = RCSwitch();
 
     //for testing purposes set to the ELRO Power Plugs
-    mySwitch.setPulseLength(300)
+    mySwitch.setPulseLength(300);
     mySwitch.enableTransmit(0);
     mySwitch.setRepeatTransmit(3);
 
--- RCSwitchJNIWrapper/src/compile.sh
+++ RCSwitchJNIWrapper/src/compile.sh
@@ -3,4 +3,4 @@
 # A shell script to compile the libs on the Pi
 
 
-g++ -I/usr/jdk1.8.0/include -I/usr/jdk1.8.0/include/linux NativeRCSwitchAdapter.cpp -o NativeRCSwitchAdapter.cpp
+g++ -shared -I/usr/jdk1.8.0/include -I/usr/jdk1.8.0/include/linux NativeRCSwitchAdapter.cpp -o NativeRCSwitchAdapter.so
