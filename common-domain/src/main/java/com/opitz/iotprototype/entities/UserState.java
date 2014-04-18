8490f9d97ab8060b77f179cc74f176c51a6a1ecc
Pascal Brokmeier <pascal.brokmeier@opitz-consulting.com>
2014-04-18 16:31:57 +0200

    updated some minor things, next commit should remove .jar again

--- common-domain/src/main/java/com/opitz/iotprototype/entities/UserState.java
+++ common-domain/src/main/java/com/opitz/iotprototype/entities/UserState.java
@@ -5,6 +5,11 @@ package com.opitz.iotprototype.entities;
  * Date: 18.04.14
  * Time: 13:29
  */
+
+
+/**
+ * duplicate can be found in oracle-cep. Please make sure to update any new required fields added to this class in the oracle-cep module as well.
+ */
 public enum UserState {
         ONLINE, OFFLINE, MISSING
 }
